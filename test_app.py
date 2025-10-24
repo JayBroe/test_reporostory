a05c67de60f9f69a2dfedf1b930b53e837787820
Jakub Brzozowski <jakub.lm.brzozowski@gmail.com>
2025-10-24 18:13:20 +0200

    Delete test_app.py

--- test_app.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from game import Block
-
-def test_block_initial_health_for_non_white_color():
-    block = Block(posx=10, posy=20, width=5, height=5, color="GREEN")
-    # jeśli kolor różny od „WHITE”, health powinien być 100
-    assert block.getHealth() == 100

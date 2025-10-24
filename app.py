13de7494eecc48a360b8a862321f4e736bda5187
Jakub Brzozowski <jakub.lm.brzozowski@gmail.com>
2025-10-24 15:49:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,14 @@ def subtract(a, b):
     """Zwraca różnicę dwóch liczb."""
     return a - b
 
+def multiply(a, b):
+    """Zwraca wynik mnożenia dwóch liczb"""
+    return a * b
 
 if __name__ == "__main__":
     print("Witaj w mojej aplikacji Python!")
     print("2 + 3 =", add(2, 3))
+
     print("5 - 1 =", subtract(5, 1))
+
+    print("2 * 2 =", add(multiply(2,2))
